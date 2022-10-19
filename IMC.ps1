f9607f1d5e5d55528dfb0edb66b4626f9bd887b2
Sergio Díaz <104896798+Navidad-xd@users.noreply.github.com>
2022-10-19 14:05:25 +0100

    Add files via upload

---
+++ IMC.ps1
@@ -0,0 +1,46 @@
+﻿<# 
+.DESCRIPTION
+Calcular el indice de masa corporal
+.EXAMPLE
+    .\IMC.ps1
+    ¿Peso (kg)? 80
+    ¿Altura (cm)? 1.76
+    Tú IMC es 25,82
+    [Bajo peso|Normal|Sobrepeso|Obeso]
+.EXAMPLE
+    .\IMC.ps1 -Peso 80 Altura 176
+    Tú IMC es 25,82
+#>
+Param(
+    [int]$peso,
+    [int] $altura
+)
+
+if ($peso -eq ""){
+    [int]$peso = Read-Host -Prompt "¿Cuál es tu peso (kg)? " 
+}
+
+if ($altura -eq ""){
+    [int]$altura = Read-Host -Prompt "¿Cuánto mides (cm)? "
+}
+
+$alturaf = $altura / 100
+$resultado = $peso/($alturaf*$alturaf)
+Write-Host "El resultado es $resultado"
+
+if ($resultado -lt 18.5){
+Write-Host "Usted está demasiado flaco"
+}
+
+elseif ($resultado -lt 24.9){
+Write-Host "Usted tiene un peso normal"
+}
+
+
+elseif ($resultado -lt 29.9){
+Write-Host "Usted tiene sobrepeso"
+}
+
+elseif ($resultado -gt 30){
+Write-Host "Usted tiene obesidad"
+}
